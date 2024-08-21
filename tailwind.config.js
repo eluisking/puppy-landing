3c78090065b44427b63b0b1a3539044e48aef5a2
eluisking <luistecisc@gmail.com>
2024-08-21 14:05:48 -0600

    [ADD] - tailwind configs colors/fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./public/index.html", "./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      'p-orange': '#fff0d9',
+      's-orange': '#e58608',
+      'p-blue': '#f6f9fe',
+      'white': '#ffffff',
+      'black': '#000000',
+    },
+    fontFamily: {
+      'poppins-r': ["Poppins", 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
